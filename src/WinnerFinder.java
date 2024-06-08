017e70d286efb02d7b828e0cd84b2e8167e65018
Darii Kinash <darkokinash@gmail.com>
2024-06-08 10:44:32 +0300

    commit #3

--- src/WinnerFinder.java
+++ src/WinnerFinder.java
@@ -13,24 +13,14 @@ public class WinnerFinder {
         board[positionData.getRow()][positionData.getCol()].getAllowedDirections().remove(direction);
         int nextRow = direction == PositionData.Direction.RIGHT ? positionData.getRow() : positionData.getRow() + 1;
         int nextCol = positionData.getCol(); // default for DOWN
-        if (direction == PositionData.Direction.RIGHT || direction == PositionData.Direction.DOWN_RIGHT) {
-            nextCol ++;
-        }
-        else if (direction == PositionData.Direction.DOWN_LEFT) {
-            nextCol --;
-        }
-        if (nextRow < 0 || nextRow >= Main.BOARD_WIDTH || nextCol < 0 || nextCol >= Main.BOARD_WIDTH) {
-            return;
-        }
-        if (board[nextRow][nextCol].getType() != positionData.getType()) {
-            return;
-        }
+        if (direction == PositionData.Direction.RIGHT || direction == PositionData.Direction.DOWN_RIGHT) nextCol ++;
+        else if (direction == PositionData.Direction.DOWN_LEFT) nextCol --;
+        if (nextRow < 0 || nextRow >= Main.BOARD_WIDTH || nextCol < 0 || nextCol >= Main.BOARD_WIDTH ||
+            board[nextRow][nextCol].getType() != positionData.getType()) return;
         positionData.incrementCount();
         positionData.setRow(nextRow);
         positionData.setCol(nextCol);
-        if (direction == PositionData.Direction.DOWN_LEFT) {
-            positionData.updateLeftmostPoint();
-        }
+        if (direction == PositionData.Direction.DOWN_LEFT) positionData.updateLeftmostPoint();
         checkNext(positionData);
     }
 
@@ -38,15 +28,12 @@ public class WinnerFinder {
         for (int row = 0; row < Main.BOARD_WIDTH; row++) {
             for (int col = 0; col < Main.BOARD_WIDTH; col++) {
                 char type = board[row][col].getType();
-                if (type == '1' || type == '2') {
-                    List<PositionData.Direction> directions = new ArrayList<>(board[row][col].getAllowedDirections());
-                    for (PositionData.Direction direction : directions) {
-                        PositionData pd = new PositionData(row, col, type, direction);
-                        checkNext(pd);
-                        if (pd.getCount() == Main.SEQUENCE_LENGTH) {
-                            return pd;
-                        }
-                    }
+                if (type != '1' && type != '2') continue;
+                List<PositionData.Direction> directions = new ArrayList<>(board[row][col].getAllowedDirections());
+                for (PositionData.Direction direction : directions) {
+                    PositionData pd = new PositionData(row, col, type, direction);
+                    checkNext(pd);
+                    if (pd.getCount() == Main.SEQUENCE_LENGTH) return pd;
                 }
             }
         }
